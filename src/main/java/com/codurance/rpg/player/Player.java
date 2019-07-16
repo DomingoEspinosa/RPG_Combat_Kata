37326bacf6b0a802380c13493e76bdc84163938a
Domingo Espinosa <domingo.espinosa@codurance.com>
2019-07-16 14:49:07 +0200

    Level test-green-refactor

--- src/main/java/com/codurance/rpg/player/Player.java
+++ src/main/java/com/codurance/rpg/player/Player.java
@@ -3,12 +3,13 @@ package com.codurance.rpg.player;
 public class Player {
 
     private int health = 1000;
+    private int level = 1;
 
     public int getHealth() {
         return health;
     }
 
     public int getLevel() {
-        return 1;
+        return level;
     }
 }
